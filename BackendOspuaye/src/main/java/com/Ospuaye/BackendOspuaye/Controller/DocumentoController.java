2fccf0863d18948ec3dde6b76e2f37d067bb93d4
Francisco <franciscobarraco5@gmail.com>
2025-08-05 20:27:57 -0300

    Termino documento service y controller

--- BackendOspuaye/src/main/java/com/Ospuaye/BackendOspuaye/Controller/DocumentoController.java
+++ BackendOspuaye/src/main/java/com/Ospuaye/BackendOspuaye/Controller/DocumentoController.java
@@ -1,13 +1,29 @@
 package com.Ospuaye.BackendOspuaye.Controller;
 
 import com.Ospuaye.BackendOspuaye.Entity.Documento;
+import com.Ospuaye.BackendOspuaye.Entity.PedidoOftalmologia;
 import com.Ospuaye.BackendOspuaye.Service.DocumentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/documentos")
 public class DocumentoController extends BaseController<Documento, Long> {
     public DocumentoController(DocumentoService service) {
         super(service);
+    }
+
+    @Autowired
+    DocumentoService documentoService;
+
+
+    @PostMapping("/cargar")
+    public ResponseEntity<String> uploadPic(@RequestParam("file") MultipartFile file) throws Exception {
+        return new ResponseEntity<>(documentoService.handleFileUpload(file), HttpStatus.OK);
     }
 }
--- BackendOspuaye/src/main/java/com/Ospuaye/BackendOspuaye/Service/DocumentoService.java
+++ BackendOspuaye/src/main/java/com/Ospuaye/BackendOspuaye/Service/DocumentoService.java
@@ -2,7 +2,15 @@ package com.Ospuaye.BackendOspuaye.Service;
 
 import com.Ospuaye.BackendOspuaye.Entity.Documento;
 import com.Ospuaye.BackendOspuaye.Repository.DocumentoRepository;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @Service
 public class DocumentoService extends BaseService<Documento, Long> {
@@ -10,4 +18,48 @@ public class DocumentoService extends BaseService<Documento, Long> {
     public DocumentoService(DocumentoRepository repository) {
         super(repository);
     }
+
+    public String handleFileUpload(MultipartFile file) throws Exception {
+        try{
+            String fileName = UUID.randomUUID().toString();
+            byte[] bytes = file.getBytes();
+            String fileOriginalName = file.getOriginalFilename();
+
+            //tamaño maximo de archivos
+            long size = file.getSize();
+            long maxSize = 5 * 1024 * 1024;
+
+            if (size > maxSize) {
+                return("El tamaño del archivo debe ser de 5MB o menor");
+            }
+
+            if (
+                    !fileOriginalName.endsWith(".jpg") &&
+                    !fileOriginalName.endsWith(".jpeg") &&
+                    !fileOriginalName.endsWith(".png") &&
+                    !fileOriginalName.endsWith(".pdf") &&
+                    !fileOriginalName.endsWith(".dox")
+
+            ){
+                return "Solo se aceptan archivos en formato JPG, JPEG, PNG, PDF, DOX";
+            }
+
+            String fileExtension = fileOriginalName.substring(fileOriginalName.lastIndexOf("."));
+            String newFileName = fileName + fileExtension;
+
+            File folder = new File("backendPasantias/BackendOspuaye/src/main/resources/pictures");
+            if(!folder.exists()){
+                folder.mkdirs();
+            }
+
+            Path path = Paths.get("backendPasantias/BackendOspuaye/src/main/resources/pictures/" + newFileName);
+            Files.write(path, bytes);
+            return "Archivo cargado correctamente";
+
+        }catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+
 }
